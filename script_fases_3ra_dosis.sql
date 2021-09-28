5413fa50234d3c9dea4560e1cff741c1c6f278da
emurcia <77987497+emurcia@users.noreply.github.com>
2021-09-28 14:58:24 -0600

    Update script_fases_3ra_dosis.sql

--- script_fases_3ra_dosis.sql
+++ script_fases_3ra_dosis.sql
@@ -53,3 +53,99 @@ left join `ds_departamento` `mndp` on
     
     
 ALTER TABLE ds_fase ADD habilitado BOOL DEFAULT false NOT NULL;
+
+
+CREATE OR REPLACE
+ALGORITHM = UNDEFINED VIEW `indice` AS
+select
+    `e`.`id` AS `id`,
+    `e`.`codigo` AS `codigo`,
+    `e`.`dui` AS `dui`,
+    `e`.`tipo_doc` AS `tipo_doc`,
+    `e`.`primer_nombre` AS `primer_nombre`,
+    `e`.`segundo_nombre` AS `segundo_nombre`,
+    `e`.`tercer_nombre` AS `tercer_nombre`,
+    `e`.`primer_apellido` AS `primer_apellido`,
+    `e`.`segundo_apellido` AS `segundo_apellido`,
+    `e`.`apellido_casada` AS `apellido_casada`,
+    concat(`e`.`primer_apellido`, ' ', coalesce(if((`e`.`apellido_casada` = ''), NULL, concat('DE ', `e`.`apellido_casada`)), coalesce(`e`.`segundo_apellido`, '')), ', ', coalesce(`e`.`primer_nombre`, ''), ' ', coalesce(`e`.`segundo_nombre`, '')) AS `nombre_completo`,
+    `e`.`fecha_nacimiento` AS `fecha_nacimiento`,
+    `e`.`id_pais_domicilio` AS `id_pais_domicilio`,
+    `e`.`id_departamento_domicilio` AS `id_departamento_domicilio`,
+    `e`.`id_municipio_domicilio` AS `id_municipio_domicilio`,
+    `e`.`id_canton_domicilio` AS `id_canton_domicilio`,
+    `e`.`id_colonia_domicilio` AS `id_colonia_domicilio`,
+    `e`.`id_establecimiento` AS `id_establecimiento`,
+    `e`.`id_fase` AS `id_fase`,
+    `e`.`calle_avenida` AS `calle_avenida`,
+    `e`.`casa_apartamento` AS `casa_apartamento`,
+    `e`.`telefono` AS `telefono`,
+    `e`.`correo` AS `correo`,
+    `ps`.`id` AS `pais/id`,
+    `ps`.`nombre` AS `pais/nombre`,
+    `ps`.`dominio2` AS `pais/dominio2`,
+    `ps`.`dominio3` AS `pais/dominio3`,
+    `mn`.`id` AS `municipio/id`,
+    `mn`.`id_departamento` AS `municipio/id_departamento`,
+    `mn`.`nombre` AS `municipio/nombre`,
+    `mn`.`abreviatura` AS `municipio/abreviatura`,
+    `mndp`.`id` AS `municipio/departamento/id`,
+    `mndp`.`nombre` AS `municipio/departamento/nombre`,
+    `mndp`.`abreviatura` AS `municipio/departamento/abreviatura`,
+    `mndp`.`id_pais` AS `municipio/departamento/id_pais`,
+    `et`.`id` AS `establecimiento/id`,
+    `et`.`nombre` AS `establecimiento/nombre`,
+    `et`.`direccion` AS `establecimiento/direccion`,
+    `etm`.`nombre` AS `establecimiento/municipio/nombre`,
+    `etd`.`nombre` AS `establecimiento/municipio/departamento/nombre`,
+    `ct`.`id` AS `canton/id`,
+    `cl`.`id` AS `colonia/id`,
+    `cl`.`id_dep` AS `colonia/id_dep`,
+    `cl`.`id_mun` AS `colonia/id_mun`,
+    `cl`.`nombre` AS `colonia/nombre`,
+    `cl`.`nombre_min` AS `colonia/nombre_min`,
+    `cl`.`nombre_may` AS `colonia/nombre_may`,
+    `e`.`img_foto` AS `foto/img_foto`,
+    `cf`.`dui` AS `confirmado/dui`,
+    `cf`.`id_cupo` AS `confirmado/id_cupo`,
+    `cf`.`fecha_cupo` AS `confirmado/fecha_cupo`,
+    `cf`.`hora_cupo` AS `confirmado/hora_cupo`,
+    `cf`.`id_departamento` AS `confirmado/id_departamento`,
+    `cf`.`id_municipio` AS `confirmado/id_municipio`,
+    `cf`.`id_colonia` AS `confirmado/id_colonia`,
+    `cf`.`id_canton` AS `confirmado/id_canton`,
+    `cf`.`calle` AS `confirmado/calle`,
+    `cf`.`casa` AS `confirmado/casa`,
+    `cf`.`estado` AS `confirmado/estado`,
+    `rv`.`id` AS `revision/id`,
+    `rv`.`estado` AS `revision/estado`,
+    `e`.`id_sexo` AS `sexo`,
+    `e`.`id_pais_nacimiento` AS `pais_nacimiento`,
+    `e`.`id_municipio_nacimiento` AS `municipio_nacimiento`,
+    `e`.`folio` AS `folio`,
+    `e`.`libro` AS `libro`,
+    `e`.`partida_nacimiento` AS `partida_nacimiento`,
+    `e`.`habilitado_3ra_dosis` AS `habilitado`,
+    `e`.`pasaporte` AS `pasaporte`
+from
+    ((((((((((`ds_elegibilidad` `e`
+left join `ds_pais` `ps` on
+    ((`e`.`id_pais_domicilio` = `ps`.`id`)))
+left join `ds_colonia` `cl` on
+    ((`e`.`id_colonia_domicilio` = `cl`.`id`)))
+left join `ds_canton` `ct` on
+    ((`e`.`id_canton_domicilio` = `ct`.`id`)))
+left join `ds_confirmados` `cf` on
+    ((`e`.`codigo` = `cf`.`codigo`)))
+left join `ds_establecimiento_fisico` `et` on
+    ((`cf`.`id_establecimiento_fisico` = `et`.`id`)))
+left join `ds_municipio` `etm` on
+    ((`etm`.`id` = `et`.`id_municipio`)))
+left join `ds_departamento` `etd` on
+    ((`etd`.`id` = `et`.`id_departamento`)))
+left join `ds_revision` `rv` on
+    ((`e`.`codigo` = `rv`.`codigo`)))
+left join `ds_municipio` `mn` on
+    ((`e`.`id_municipio_domicilio` = `mn`.`id`)))
+left join `ds_departamento` `mndp` on
+    ((`mn`.`id_departamento` = `mndp`.`id`)))
